8c3067f38aef85cffcdb1f1b0f9d3f3a79ae0cb3
Ben Dreyer <bendreyer21@gmail.com>
2021-05-17 19:38:59 -0600

    team float fix

--- sportSite/settings.py
+++ sportSite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .secret_keys import django_key
+from .secret_keys import django_key, db_password
 SECRET_KEY = django_key
 
 
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sportsdb',
         'USER': 'admin',
-        'PASSWORD': 'admin12345',
+        'PASSWORD': db_password,
         'HOST': 'localhost',
         'PORT': '',
     }
